344584712d6b8a3bdfe318960a667f322cf0bd1b
LahmanetsII <LahmanetsII@skblab.ru>
2026-04-20 20:46:36 +0500

    unknown error [1]

--- src/test/java/abs/zer0/KafkaTest.java
+++ src/test/java/abs/zer0/KafkaTest.java
@@ -1,17 +1,22 @@
 package abs.zer0;
 
+import abs.zer0.data.InternalServerError;
 import io.micronaut.configuration.kafka.annotation.KafkaListener;
 import io.micronaut.configuration.kafka.annotation.Topic;
+import io.micronaut.core.type.Argument;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,6 +25,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest
 public class KafkaTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaTest.class);
 
     private static final String TOPIC_NAME_1 = "test-topic.1";
     private static final String TOPIC_NAME_2 = "another-topic.2";
@@ -33,13 +40,16 @@ public class KafkaTest {
     @Test
     void sendMessage(TestConsumer consumer) {
         final HttpRequest<String> request = HttpRequest.POST(TOPIC_NAME_1, MESSAGE_BODY)
-                .contentType(MediaType.TEXT_PLAIN_TYPE)
-                .accept(MediaType.APPLICATION_JSON_TYPE);
-        final HttpResponse<String> response = httpClient.toBlocking().exchange(request, String.class);
+                .contentType(MediaType.TEXT_PLAIN_TYPE);
+        try {
+            final HttpResponse<String> response = httpClient.toBlocking().exchange(request, String.class);
 
-        assertEquals(HttpStatus.OK, response.status());
-        assertNotNull(response.body());
-        assertTrue(response.body().contains(TOPIC_NAME_1));
+            assertEquals(HttpStatus.OK, response.status());
+            assertNotNull(response.body());
+            assertTrue(response.body().contains(TOPIC_NAME_1));
+        } catch (HttpClientResponseException hcre) {
+            LOG.error("Error", hcre);
+        }
 
         await()
                 .atMost(10, TimeUnit.SECONDS)
